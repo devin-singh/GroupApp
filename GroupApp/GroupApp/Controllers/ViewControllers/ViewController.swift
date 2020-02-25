67a89c7643d09cb791b8b96e5725ff1c95e81700
Devin Singh <notpublic@gmail.com>
2020-02-25 10:30:47 -0700

    Added project file structure

---
+++ GroupApp/GroupApp/Controllers/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupApp
+//
+//  Created by Devin Singh on 2/25/20.
+//  Copyright © 2020 Devin Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GroupApp/GroupApp/Models/Profile.swift
@@ -0,0 +1,9 @@
+//
+//  Profile.swift
+//  GroupApp
+//
+//  Created by Devin Singh on 2/25/20.
+//  Copyright © 2020 Devin Singh. All rights reserved.
+//
+
+import Foundation
